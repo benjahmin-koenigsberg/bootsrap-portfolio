bf63e71d17db679081998352d6b1dc9ebfe328bd
Benjahmin Koenigsberg <magnanimousplayer@gmail.com>
2023-10-09 14:21:19 -0400

    adds typing effect in javascript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-// Add custom JavaScript here
+// typing effect
+
+function runTypingEffect() {
+    const text = 'I am Benjahmin Koenigsberg.';
+    const typingElement = document.getElementById('typing-text');
+    const typingDelay = 125;
+
+    typeText(text, typingElement, typingDelay)
+
+}
+
+
+function typeText(text, element, delay) {
+
+    for (let i = 0; i < text.length; i++) {
+        setTimeout(() => {
+            element.textContent += text.charAt(i);
+        }, delay * i)
+    }
+}
+
+document.addEventListener('DOMContentLoaded', runTypingEffect);
