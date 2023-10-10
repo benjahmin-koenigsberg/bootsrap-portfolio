49ec071acb642746aadb5e594c1a677eb5fb38f0
Benjahmin Koenigsberg <magnanimousplayer@gmail.com>
2023-10-09 20:58:47 -0400

    trouble shoot lightbox for mobile

--- js/script.js
+++ js/script.js
@@ -1,22 +1,31 @@
-// typing effect
-function runTypingEffect() {
-    const text = 'I am Benjahmin Koenigsberg.';
-    const typingElement = document.getElementById('typing-text');
-    const typingDelay = 125;
+//typing effect variables
+const text = 'I am Benjahmin Koenigsberg.';
+const typingElement = document.getElementById('typing-text');
+const typingDelay = 100;
 
+//run typing function
+function runTypingEffect() {
     typeText(text, typingElement, typingDelay)
 
+    //run repeat typing function
+
+   // setTimeout(repeatType, 5500)
 }
 
-
+// typing effect function
 function typeText(text, element, delay) {
-
     for (let i = 0; i < text.length; i++) {
         setTimeout(() => {
             element.textContent += text.charAt(i);
         }, delay * i)
     }
-
 }
 
+//rerun typing efect function
+function repeatType(){
+        typingElement.textContent = '';
+        runTypingEffect();
+}
+
+//auto run function on page load
 document.addEventListener('DOMContentLoaded', runTypingEffect);
